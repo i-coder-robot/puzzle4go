ef5c2b137860047aa1138482e00718cd8df678f2
Monster <coolpest@gmail.com>
2020-08-05 07:32:13 +0800

    done

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 100道大厂Go语言面试题
 
+整理更新，陆续放出新增面试题，共勉，面向加薪学习!
+
  [视频讲解](https://www.bilibili.com/video/BV1bv411v7fQ/)
--- puzzle30/main.go
+++ puzzle30/main.go
@@ -1,43 +1,14 @@
 package main
 
-import "fmt"
-
 //[intermediate] 关于接口，下面说法正确的是（）
 //A. 只要两个接口拥有相同的方法列表（次序不同不要紧），那么它们就是等价的，可以相互赋值
 //B. 如果接口A的方法列表是接口B的方法列表的子集，那么接口B可以赋值给接口A
 //C. 接口查询是否成功，要在运行期才能够确定
 //D. 接口赋值是否可行，要在运行期才能够确定
-//参考答案：ACD
+//参考答案：ABCD
 
-//CD看着很顺眼
-
-//A 没问题，如果你有问题，可以一下，下面的例子
-//B这不用说，肯定是不行的啊 可以看下面的例子
-
-type Work interface {
-	DoWork()
-	Eat()
-	Sleep()
-}
-
-type Life interface {
-	Sleep()
-	DoWork()
-	Eat()
-
-}
-
-type Son interface {
-	Sleep()
-	Eat()
-}
+//A儿子  B爹   父给子转换
 
 func main() {
-	var w Work
-	var l Life
-	w=l
-	fmt.Println(w)
 
-	var s Son
-	l=s
 }
